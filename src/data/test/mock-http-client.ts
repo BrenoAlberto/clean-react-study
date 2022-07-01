b5c27d728e86a4188950b5f325fe4a2bc0c3ba7c
Breno Alberto <brenoaps435@gmail.com>
2022-07-01 09:17:12 -0300

    feat: ensure HttpPostClient is called with correct body

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -2,9 +2,11 @@ import { HttpPostClient, HttpPostParams } from 'data/protocols/http/http-post-cl
 
 export class HttpPostClientSpy implements HttpPostClient {
   url?: string
+  body?: object
 
   post = async (params: HttpPostParams): Promise<void> => {
     this.url = params.url
+    this.body = params.body
     return await Promise.resolve()
   }
 }
